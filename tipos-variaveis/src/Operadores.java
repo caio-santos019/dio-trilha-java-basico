1ec24b6c6d0d6fd6ad02728e9583b70ebdae1ac7
caio-santos019 <caio.leme10@gmail.com>
2024-03-18 08:13:34 -0300

    condicional ternario

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,21 +1,42 @@
 public class Operadores {
     public static void main(String[] args) {
-
         int numero = 5;
 
-        numero = -numero;
+        // Imprimindo o numero negativo
+        System.out.println(-numero);
 
+        // incrementando numero em mais 1 numero, imprime 6
+        numero++;
         System.out.println(numero);
 
-        // forma incorreta de transformar um numero negativo em positivo
-        numero = +numero;
+        // incrementando numero em mais 1 numero, imprime 7
+        System.out.println(numero++);// ops algo de errado não está certo
 
-        System.out.println(numero);
+        System.out.println(numero);// agora sim, numero virou 7
 
-        // forma correta
-        numero = numero * -1;
+        // ordem de precedencia conta aqui
+        System.out.println(++numero);
 
-        System.out.println(numero);
+        boolean verdadeiro = true;
+
+        System.out.println("Inverteu " + !verdadeiro);
+
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        /*
+         * EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+         * if(a==b)
+         * resultado = "verdadeiro";
+         * else
+         * resultado = "falso";
+         */
+
+        // MESMA CONDICIONAL, MAS DESSA VEZ, UTILIZANDO O OPERADOR CONDICIONAL TERNÁRIO
+        String resultado = a == b ? "verdadeiro" : "falso";
+        System.out.println(resultado);
 
     }
 }
