cdeaf5259dc936636553ad779b7723ddc071e8d9
caio-santos019 <caio.leme10@gmail.com>
2024-03-18 07:12:54 -0300

    tipos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,21 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+
+        System.out.println(numero);
+
+        /*
+         * Para que o valor de uma váriavel nunca seja alterado
+         * é necessário que a declaração seja feita em caixa alta
+         * e se utilize a palavra "final" no início.
+         */
+        final double VALOR_DE_PI = 3.14;
     }
 }
