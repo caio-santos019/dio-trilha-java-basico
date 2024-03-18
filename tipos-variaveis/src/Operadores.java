e7d023b97320acd9ac91292779c15656bf981526
caio-santos019 <caio.leme10@gmail.com>
2024-03-18 07:44:24 -0300

    conversao de numeros

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,34 +1,21 @@
 public class Operadores {
     public static void main(String[] args) {
 
-        /*
-         * String nomeCompleto = "LINGUAGEM " + "JAVA";
-         * System.out.println(nomeCompleto);
-         */
+        int numero = 5;
 
-        String concatenacao = "?";
+        numero = -numero;
 
-        System.out.println(concatenacao);
+        System.out.println(numero);
 
-        concatenacao = 1 + 1 + 1 + "1";
+        // forma incorreta de transformar um numero negativo em positivo
+        numero = +numero;
 
-        System.out.println(concatenacao);
+        System.out.println(numero);
 
-        concatenacao = 1 + "1" + 1 + 1;
+        // forma correta
+        numero = numero * -1;
 
-        System.out.println(concatenacao);
-
-        concatenacao = 1 + "1" + 1 + "1";
-
-        System.out.println(concatenacao);
-
-        concatenacao = "1" + 1 + 1 + 1;
-
-        System.out.println(concatenacao);
-
-        concatenacao = "1" + (1 + 1 + 1);
-
-        System.out.println(concatenacao);
+        System.out.println(numero);
 
     }
 }
