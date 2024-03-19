1bb5b9764f44ac4eebb0f502971ca8ad3e4b1164
caio-santos019 <caio.leme10@gmail.com>
2024-03-18 22:10:28 -0300

    finalizacao de operadores

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -38,5 +38,40 @@ public class Operadores {
         String resultado = a == b ? "verdadeiro" : "falso";
         System.out.println(resultado);
 
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        if (numero1 == numero2) { // true
+            System.out.println("A nossa condição é verdadeira");
+        }
+
+        System.out.println("O número 1 é igual ao numero 2? " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("O número 1 é diferente do numero 2? " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("O número 1 é maior do que o numero 2? " + simNao);
+
+        String nomeUm = "CAIO";
+        String nomeDois = new String("CAIO");
+
+        System.out.println(nomeUm.equals(nomeDois));
+
+        boolean condicao1 = true;
+        boolean condicao2 = false;
+
+        if (condicao1 && condicao2) {
+            System.out.println("As duas condições são verdadeiras");
+        }
+
+        if (condicao1 || condicao2) {
+            System.out.println("Uma das condições é verdadeira");
+        }
+
     }
 }
